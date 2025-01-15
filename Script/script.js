2d751f3480431a46bb33c42141455fc44a7da2ee
TomH999 <thacot008@gmail.com>
2025-01-15 16:49:10 +0100

    Create script.js

---
+++ Script/script.js
@@ -0,0 +1,5 @@
+// JavaScript pour ajouter des interactions futures
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Page chargée et prête !");
+    // Vous pouvez ajouter des animations ou d'autres fonctionnalités ici
+});
